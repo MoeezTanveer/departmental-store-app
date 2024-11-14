d0f75e0adc10377c86580d85bbfc4b213b755b5e
Admin <grup3@ncba.com>
2024-11-14 16:14:35 +0500

    commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white text-center p-4">
+      <h1>Departmental Store Application</h1>
+    </div>
+  );
+}
+export default App;
